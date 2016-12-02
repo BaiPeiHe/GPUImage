769993591858cd8d9e712ebdc7b9f06ea6cdde6d
白鹤 <18346669516@163.com>
2016-12-02 12:27:41 +0800

    低 - 调整界面,接入 cocoa pod

---
+++ gpuimage/Controller/RootViewController.m
@@ -0,0 +1,40 @@
+//
+//  RootViewController.m
+//  gpuimage
+//
+//  Created by 白鹤 on 16/12/2.
+//  Copyright © 2016年 白鹤. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor brownColor];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
